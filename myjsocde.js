56453b7f05771414a85a028ee98f2d284f1c447c
Saad_Khan <Khan.ji4028@gmail.com>
2021-03-21 14:36:45 +0500

    update

--- myjsocde.js
+++ myjsocde.js
@@ -18,16 +18,21 @@ function handleFormSubmit(e) {
 function nameCheck() {
   if ($("#name").val().length < 5) {
     $("#name").addClass("error");
-  } else $("#name").removeClass("error");
-
+    $("#error_name").html("Name must be 5 character long");
+  } else {$("#name").removeClass("error");
+  $("#error_name").html(" ");
+  }
   return;
 }
 
 function emailCheck() {
   if (!$("#email").val().includes("@") || !$("#email").val().includes(".")) {
     $("#email").addClass("error");
+    $("#error_email").html("Enter email in correct format");
+
   } else {
     $("#email").removeClass("error");
+    $("#error_email").html(" ");
   }
   return;
 }
@@ -35,20 +40,23 @@ function emailCheck() {
 function passwordCheck() {
   if ($("#password").val().length < 8) {
     $("#password").addClass("error");
+    $("#error_password").html("Password must be 8 character long");
   } else {
     $("#password").removeClass("error");
+    $("#error_password").html(" ");
   }
   return;
 }
 
 function confirmPasswordCheck() {
   if (
-    $("#confirm_password").val() != $("#password").val() ||
-    $("#confirm_password").val().length < 8
+    $("#confirm_password").val() != $("#password").val()
   ) {
     $("#confirm_password").addClass("error");
+    $("#error_cpassword").html("Password must be same as above");
   } else {
     $("#confirm_password").removeClass("error");
+    $("#error_cpassword").html(" ");
   }
   return;
 }
