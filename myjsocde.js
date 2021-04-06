a4e2fd4d9a258a5c21752d7aa1f9f0d350725fa6
Saad_Khan <Khan.ji4028@gmail.com>
2021-04-06 16:03:08 +0500

    update

--- myjsocde.js
+++ myjsocde.js
@@ -10,9 +10,11 @@ $(function () {
 function handleFormSubmit(e) {
   if (
     !(nameCheck() && emailCheck() && passwordCheck() && confirmPasswordCheck())
-  )
-    return false;
-  alert("Form is being submitted");
+  ) {
+    alert("Form is not being submitted");
+  } else {
+    alert("Form is being submitted");
+  }
   e.preventDefault();
 }
 
@@ -20,42 +22,46 @@ function nameCheck() {
   if ($("#name").val().length < 5) {
     $("#name").addClass("error");
     $("#error_name").show();
+    return false;
   } else {
     $("#name").removeClass("error");
     $("#error_name").hide();
+    return true;
   }
-  return;
 }
 
 function emailCheck() {
   if (!$("#email").val().includes("@") || !$("#email").val().includes(".")) {
     $("#email").addClass("error");
     $("#error_email").show();
+    return false;
   } else {
     $("#email").removeClass("error");
     $("#error_email").hide();
+    return true;
   }
-  return;
 }
 
 function passwordCheck() {
   if ($("#password").val().length < 8) {
     $("#password").addClass("error");
     $("#error_password").show();
+    return false;
   } else {
     $("#password").removeClass("error");
     $("#error_password").hide();
+    return true;
   }
-  return;
 }
 
 function confirmPasswordCheck() {
   if ($("#confirm_password").val() != $("#password").val()) {
     $("#confirm_password").addClass("error");
     $("#error_cpassword").show();
+    return false;
   } else {
     $("#confirm_password").removeClass("error");
     $("#error_cpassword").hide();
+    return true;
   }
-  return;
 }
